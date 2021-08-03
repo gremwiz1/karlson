8473d9cdf93755da929f840b35e9c1ab20685199
Gremwiz1 <gremwiz@yandex.ru>
2021-08-03 23:49:25 +0300

    Added checkbox image and set checkbox

--- src/components/Panels.js
+++ src/components/Panels.js
@@ -4,8 +4,12 @@ import PanelNumberTelephone from './PanelNumberTelephone';
 function Panels() {
 
     const [numberTelephone, SetNumberTelephone] = React.useState([]);
-
-
+    const [isChecked, setIsChecked] = React.useState(false);
+    function handleCheckBox() {
+        const el = document.querySelector('.checkbox_invisible');
+        el.checked = !isChecked;
+        setIsChecked(!isChecked);
+    }
     function pressKeyboardTelephone(number) {
 
         SetNumberTelephone(prevState => {
@@ -25,7 +29,7 @@ function Panels() {
             <div className="panels__agreement">
                 <label className="checkbox">
                     <input type="checkbox" class="checkbox_invisible" id="1" name="checkbox" />
-                    <div class="checkbox_visible"></div>
+                    <div className={isChecked ? "checkbox_visible checkbox_checked" : "checkbox_visible"} onClick={handleCheckBox}></div>
                 </label>
                 <p className="checkbox__text">Согласие на обработку персональных данных</p>
             </div>
